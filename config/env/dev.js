82fdd09e746d7b30a0609c31a2af9a5b133d388a
NickAMars <niclaus.a@gmail.com>
2022-02-08 14:42:51 -0500

    seperating configuration from application

---
+++ config/env/dev.js
@@ -0,0 +1,4 @@
+module.exports = {
+  MONGODBURL:
+    "mongodb+srv://nick:password-1@groupcolor.tiw5h.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+};
---
+++ config/env/test.js
@@ -0,0 +1,4 @@
+module.exports = {
+  MONGODBURL:
+    "mongodb+srv://nick:password-1@groupcolor.xlplu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+};
